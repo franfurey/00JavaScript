41cacf7c24dcfde68f54d57376c37c4f62eaf3d8
Francisco Furey <franciscofurey@gmail.com>
2022-08-16 16:25:01 -0300

    1 Entregable

--- 00entregables/01entregableFranFurey/01entregableFranciscoFurey.js
+++ 00entregables/01entregableFranFurey/01entregableFranciscoFurey.js
@@ -6,34 +6,3 @@ let num = parseInt(prompt('Ingrese un numero'));
 for (let i = 0; i <= 1000; i = i + num) {
     console.log(i)
 }
-
-
-let today = new Date();  // me entrega la fecha de hoy. 
-let day = today.getDay();
-
-switch (day) {
-    case 1:
-        console.log('Es Lunes')
-        break;
-    case 2:
-        console.log('Es martes')
-        break;
-    case 3:
-        console.log('Es miercoles')
-        break;
-    case 4:
-        console.log('Es jueves')
-        break;
-    case 5:
-        console.log('Es viernes')
-        break;
-    case 6:
-        console.log('Es sabado')
-        break;
-    case 7:
-        console.log('Es domingo')
-        break;
-
-    default:
-        break;
-}

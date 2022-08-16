72dd8f22527767447fb2872b0ccc848cc60ae158
Francisco Furey <franciscofurey@gmail.com>
2022-08-16 16:22:20 -0300

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let comillasSimples = 'comillasSimples';
+
+let comillasDobles = 'comillasDobles';
+
+let backticks = `
+    string backticks    
+`;
+
+let entero = 555;
+
+console.log(backticks+ ' '+ comillasDobles + " " + entero);
+
+let entrada =prompt('Ingresar una numero');
+
+alert(parseInt(entrada)*20 + ' multiplicacion efectuada');

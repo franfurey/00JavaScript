6f9543ef03f9d6f36a366d67a2ab881df3177ead
Francisco Furey <franciscofurey@gmail.com>
2022-08-16 16:15:47 -0300

    Initial commit

---
+++ ejercicio.js
@@ -0,0 +1,7 @@
+const username = prompt('Ingrese el username')
+
+const password = prompt('Ingrese la constraseña')
+
+const VALUE = 42069
+
+alert(username + VALUE + password)
---
+++ ejercico2.js
@@ -0,0 +1,19 @@
+const madre = parseInt(prompt('Ingrese su edad madre'));
+
+const padre = parseInt(prompt('Ingrese su edad padre'));
+
+const hermano = parseInt(prompt('Ingrese su edad hermano'));
+
+const hermana = parseInt(prompt('Ingrese su edad hermana'));
+
+const promedio = (madre + padre+ hermana + hermano)/4;
+
+alert('La edad media de mi familia es: ' + promedio);
+
+alert('madre tiene en 7 años: ' + parseInt(madre+7));
+
+alert('padre tiene en 7 años: ' + parseInt(padre+7));
+
+alert('hermano: ' + parseInt(hermano+7));
+
+alert('hermana: ' + parseInt(hermana+7));

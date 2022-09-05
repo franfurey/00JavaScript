3309a5c63940ab8847d12e1e9a714e579476bf4f
Francisco Furey <franciscofurey@gmail.com>
2022-09-05 13:40:15 -0300

    entregable

--- 00entregables/03entregable/03entregableFranciscoFurey.js
+++ 00entregables/03entregable/03entregableFranciscoFurey.js
@@ -21,43 +21,5 @@ for (let i = 0; i < gastoTotal.length; i++) {
 }
 console.log("El monto total gastado es de", totalGastado, "pesos argentinos");
 
-console.log("El mayor gasto producido fue de:", Math.max(gastoTotal));
-console.log("El menor gasto producido fue de:", Math.min(gastoTotal));
-
-// let gastosTotales2 = [
-//   {
-//     id: 1,
-//     gastoTotal2: 2000,
-//     responsableGasto2: "Francisco",
-//     descripcion: "Materiales de produccion",
-//   },
-//   {
-//     id: 2,
-//     gastoTotal2: 1500,
-//     responsableGasto2: "Francisco",
-//     descripcion: "Materiales de produccion",
-//   },
-//   {
-//     id: 3,
-//     gastoTotal2: 3000,
-//     responsableGasto2: "Cecilia",
-//     descripcion: "Herramientas",
-//   },
-//   {
-//     id: 4,
-//     gastoTotal2: 5000,
-//     responsableGasto2: "Ulises",
-//     descripcion: "Herramientas",
-//   },
-//   {
-//     id: 5,
-//     gastoTotal2: 10000,
-//     responsableGasto2: "Cecilia",
-//     descripcion: "Insumos de limpieza",
-//   },
-// ];
-
-// console.log(
-//   "El mayor gasto producido fue de:",
-//   Math.max(gastosTotales.gastoTotal2)
-// );
+console.log("El mayor gasto producido fue de:", Math.max(...gastoTotal));
+console.log("El menor gasto producido fue de:", Math.min(...gastoTotal));

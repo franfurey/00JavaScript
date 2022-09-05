d3f8cf5b173adf148fdc6f8013a00a0cc8f947f3
Francisco Furey <franciscofurey@gmail.com>
2022-09-05 19:53:44 -0300

    entregable

--- 00entregables/03entregable/03entregableFranciscoFurey.js
+++ 00entregables/03entregable/03entregableFranciscoFurey.js
@@ -23,3 +23,45 @@ console.log("El monto total gastado es de", totalGastado, "pesos argentinos");
 
 console.log("El mayor gasto producido fue de:", Math.max(...gastoTotal));
 console.log("El menor gasto producido fue de:", Math.min(...gastoTotal));
+
+let contador = 0;
+let h2Contador = document.querySelector("#contador");
+h2Contador.innerHTML = contador;
+let inputReset = document.querySelector("#reset");
+inputReset.addEventListener("change", resetToNumber);
+
+function actualizarContador() {
+  let h2Contador = document.querySelector("#contador");
+  h2Contador.innerHTML = contador;
+}
+function incrementar() {
+  contador++;
+  actualizarContador();
+}
+function decrementar() {
+  contador--;
+  actualizarContador();
+}
+
+function reset() {
+  contador = 0;
+  actualizarContador();
+}
+
+function resetToNumber() {
+  const valueToReset = inputReset.value;
+  contador = parseInt(valueToReset);
+  actualizarContador();
+}
+
+function addText(texto) {
+  let textoDiv = document.querySelector("#texto");
+  let h2Created = document.createElement("h2");
+  textoDiv.append(h2Created);
+  h2Created.innerHTML = texto;
+}
+
+function removeText() {
+  let h2Created = document.querySelector("#texto h2");
+  h2Created.remove();
+}

524065eb3ec06ab5cb875c3333a61470e8d3d82c
Francisco Furey <franciscofurey@gmail.com>
2022-09-16 10:01:14 -0300

    entregabke

--- 00final/index.js
+++ 00final/index.js
@@ -1,47 +1,47 @@
-// let formulario = document.querySelector("#login");
+let formulario = document.querySelector("#login");
 
-// function login(event) {
-//   event.preventDefault();
-//   let valueUsername = formulario.username.value;
-//   let valuePassword = formulario.password.value;
+function login(event) {
+  event.preventDefault();
+  let valueUsername = formulario.username.value;
+  let valuePassword = formulario.password.value;
 
-//   const objetoUsuario = {
-//     username: valueUsername,
-//     password: valuePassword,
-//   };
+  const objetoUsuario = {
+    username: valueUsername,
+    password: valuePassword,
+  };
 
-//   const token = generateToken(objetoUsuario.username, objetoUsuario.password);
-//   localStorage.setItem("token", token);
-//   const isAuth = existToken();
-//   if (isAuth) {
-//     const divPrivateContent = document.querySelector("#privateContent");
-//     divPrivateContent.className = "showContent";
-//     formulario.className = "hideContent";
-//     showUsername();
-//   }
-// }
+  const token = generateToken(objetoUsuario.username, objetoUsuario.password);
+  localStorage.setItem("token", token);
+  const isAuth = existToken();
+  if (isAuth) {
+    const divPrivateContent = document.querySelector("#privateContent");
+    divPrivateContent.className = "showContent";
+    formulario.className = "hideContent";
+    showUsername();
+  }
+}
 
-// function generateToken(username, password) {
-//   const usuario = {
-//     username,
-//     password,
-//   };
-//   return JSON.stringify(usuario);
-// }
+function generateToken(username, password) {
+  const usuario = {
+    username,
+    password,
+  };
+  return JSON.stringify(usuario);
+}
 
-// function existToken() {
-//   if (localStorage.getItem("token") !== null) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function existToken() {
+  if (localStorage.getItem("token") !== null) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// function showUsername() {
-//   const strong = document.querySelector("#usernameLogin");
-//   const token = JSON.parse(localStorage.getItem("token"));
-//   strong.innerHTML = token.username;
-// }
+function showUsername() {
+  const strong = document.querySelector("#usernameLogin");
+  const token = JSON.parse(localStorage.getItem("token"));
+  strong.innerHTML = token.username;
+}
 
 class Expense {
   constructor(id, description, person, amount) {

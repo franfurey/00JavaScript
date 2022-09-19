ec3701f8cfbbc7ffb3b55065e921ee018e12e6c3
Francisco Furey <franciscofurey@gmail.com>
2022-09-19 13:38:41 -0300

    '#'

--- 00final/index.js
+++ 00final/index.js
@@ -127,6 +127,19 @@ function updateExpenseTable() {
 }
 
 function deleteExpense(event) {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire("Deleted!", "Your file has been deleted.", "success");
+    }
+  });
   const btn = event.target;
   const id = btn.id.split("_")[1];
   expense = expense.filter((expense) => expense.id != id);
